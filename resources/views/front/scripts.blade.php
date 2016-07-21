5b17649db66ab5fff9e396a375720464fbf4b706
InterProFront <fallzem@gmail.com>
2016-07-21 18:22:48 +0600

    fix google map

--- resources/views/front/scripts.blade.php
+++ resources/views/front/scripts.blade.php
@@ -1,6 +1,6 @@
 @section('scripts')
-    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCrLMaG8RXAt2WWfxArt-7qXJFGRdVyq7g&callback=initMap"></script>
     <script src="/js/jquery.js"></script>
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCrLMaG8RXAt2WWfxArt-7qXJFGRdVyq7g&callback=initMap"></script>
     <script src="/js/TweenMax.min.js"></script>
     <script src="/js/jquery.magnific-popup.js"></script>
     <script src="/js/main.js"></script>
