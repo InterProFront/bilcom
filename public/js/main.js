906411fb2535feb001d2521b4173f96d79142c4b
InterProFront <fallzem@gmail.com>
2016-07-21 18:09:28 +0600

    fix google map

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,20 @@
+var map;
+var geocoder;
+function initMap() {
+    map = new google.maps.Map( document.getElementById('map_cart'), {
+        center: {lat: 43.257487, lng: 76.9392692},
+        zoom: 14
+    });
+    geocoder = new google.maps.Geocoder();
+    google.maps.event.trigger(map, 'resize');
+    map.addListener('click',function(event){
+        geocoder.geocode({'latLng': event.latLng},function(result,status){
+            //alert(result[0].formatted_address)
+            $('.popup_field[data-field-name="address"]').val(result[0].formatted_address).attr('title',result[0].formatted_address);
+            $.magnificPopup.close();
+        });
+    });
+}
 
 $(document).ready(function () {
     $('.map-button').magnificPopup({
@@ -6,23 +23,7 @@ $(document).ready(function () {
         callbacks: {
 
             open: function(){
-                var map;
-                var geocoder;
-                function initMap() {
-                    map = new google.maps.Map( document.getElementById('map_cart'), {
-                        center: {lat: 43.257487, lng: 76.9392692},
-                        zoom: 14
-                    });
-                    geocoder = new google.maps.Geocoder();
-                    google.maps.event.trigger(map, 'resize');
-                    map.addListener('click',function(event){
-                        geocoder.geocode({'latLng': event.latLng},function(result,status){
-                            //alert(result[0].formatted_address)
-                            $('.popup_field[data-field-name="address"]').val(result[0].formatted_address).attr('title',result[0].formatted_address);
-                            $.magnificPopup.close();
-                        });
-                    });
-                }
+
 
 
             }
